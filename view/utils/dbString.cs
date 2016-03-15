c09795f9dad504e25dc81c8cd67b0f6c0f331f36
travel0891 <travel4ever@yeah.net>
2016-03-15 18:55:31 +0800

    format code

--- view/utils/dbString.cs
+++ view/utils/dbString.cs
@@ -7,18 +7,34 @@ namespace view
         {
             switch (sqlType.ToLower())
             {
-                case "int": return "Int32";
-                case "smallint": return "Int16";
-                case "smalldatetime": return "DateTime";
-                case "datetime": return "DateTime";
-                case "char": return "String";
-                case "varchar": return "String";
-                case "text": return "String";
-                case "nchar": return "String";
-                case "nvarchar": return "String";
-                case "ntext": return "String";
-                case "uniqueidentifier": return "String";
-                default: return "string";
+                case "int":
+                    return "Int32";
+                case "smallint":
+                    return "Int16";
+
+                case "smalldatetime":
+                    return "DateTime";
+                case "datetime":
+                    return "DateTime";
+
+                case "char":
+                    return "String";
+                case "varchar":
+                    return "String";
+                case "text":
+                    return "String";
+
+                case "nchar":
+                    return "String";
+                case "nvarchar":
+                    return "String";
+                case "ntext":
+                    return "String";
+
+                case "uniqueidentifier":
+                    return "String";
+                default:
+                    return "doCheck";
             }
         }
 
@@ -26,18 +42,34 @@ namespace view
         {
             switch (sqlType.ToLower())
             {
-                case "int": return "GetInt32";
-                case "smallint": return "GetInt16";
-                case "smalldatetime": return "GetDateTime";
-                case "datetime": return "GetDateTime";
-                case "char": return "GetString";
-                case "varchar": return "GetString";
-                case "text": return "GetString";
-                case "nchar": return "GetString";
-                case "nvarchar": return "GetString";
-                case "ntext": return "GetString";
-                case "uniqueidentifier": return "GetGuid";
-                default: return "GetString";
+                case "int":
+                    return "GetInt32";
+                case "smallint":
+                    return "GetInt16";
+
+                case "smalldatetime":
+                    return "GetDateTime";
+                case "datetime":
+                    return "GetDateTime";
+
+                case "char":
+                    return "GetString";
+                case "varchar":
+                    return "GetString";
+                case "text":
+                    return "GetString";
+
+                case "nchar":
+                    return "GetString";
+                case "nvarchar":
+                    return "GetString";
+                case "ntext":
+                    return "GetString";
+
+                case "uniqueidentifier":
+                    return "GetGuid";
+                default:
+                    return "doCheck";
             }
         }
 
